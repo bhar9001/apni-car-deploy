ddc904c42b3d86e267d90fdcce4cd4d82e0b6704
Bharat Suthar <103443194+bhar9001@users.noreply.github.com>
2023-07-23 17:31:53 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/cars/", require("./routes/carsRoute"));
 app.use("/api/users/", require("./routes/usersRoute"));
