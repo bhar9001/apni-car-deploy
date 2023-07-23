16a50a585ea0f006e4a4576fa21d54f54ef92d08
Bharat Suthar <103443194+bhar9001@users.noreply.github.com>
2023-07-23 18:24:07 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 
 app.use(express.json());
@@ -9,6 +10,13 @@ app.use(cors());
 app.use("/api/cars/", require("./routes/carsRoute"));
 app.use("/api/users/", require("./routes/usersRoute"));
 app.use("/api/bookings/", require("./routes/bookingsRoute"));
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// Serve the index.html file for all non-API routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 // MIDDLEWARES
 console.log(process.env.NODE_ENV);
